a9e45ff1bf609b159ba13dfeb68ab91d7f46232d
Dikshantashresth <shresthadikshanta25@gmail.com>
2025-07-08 14:48:16 +0545

    v1.2

--- src/components/Logginout.jsx
+++ src/components/Logginout.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 
 import { useNavigate } from 'react-router-dom';
 import axios from '../utils/axios'
+import { useState } from 'react';
 const Logginout = () => {
    const navigate = useNavigate();
 
