feead7db45e8bc3ef51dbfef4e3b70251ab43d14
Dikshantashresth <shresthadikshanta25@gmail.com>
2025-07-08 12:53:01 +0545

    update myrecipies

--- src/components/MyRecipie.jsx
+++ src/components/MyRecipie.jsx
@@ -4,16 +4,23 @@ import MyRecipieCard from './MyRecipieCard'
 
 const MyRecipie = () => {
     const [yourRecipie, setRecipie] = useState([]);
+
     useEffect(()=>{
         const fetch = async ()=>{
             const data = await axios.get('/myrecipies',{withCredentials:true})
             if(data){
                 setRecipie(data.data);
-                console.log(data.data)
+                
             }
         }
         fetch();
-    },[])
+    },[]);
+     const deletefromdatabase = async ({id})=>{
+            await axios.delete(`/delete/${id}`,{withCredentials:true})
+        }
+        const handleDelete = ()=>{
+          deletefromdatabase();
+        }
   return (
     <div>
         <h3 className='text-white font-bold mt-5 text-4xl   mb-4'>
@@ -21,7 +28,7 @@ const MyRecipie = () => {
           </h3>
       <div className='flex flex-wrap gap-10 justify-center '>
         {yourRecipie.length > 0 ? (
-          yourRecipie.map((recipie, i) =>  <div className="w-[300px] flex"> <MyRecipieCard key={i} recipie={recipie} /></div>)
+          yourRecipie.map((recipie, i) =>  <div className="w-[300px] flex"> <MyRecipieCard key={i} recipie={recipie} handler={handler} /></div>)
         ) : (
           <p className='text-white'>No results found.</p>
         )}
